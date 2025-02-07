f8ca5c79e3d933600bf4b3ba595b6eecadb92549
Tobaltguino <crstbl.ihg@gmail.com>
2025-02-07 06:31:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const btnNo = document.querySelector("#btn-random")
+const btnNo = document.querySelector("#btn-random");
 
 function moverAleatoriamente(btn) {
     btn.style.position = "absolute";
     btn.style.fontWeight = "bolder";
-    btn.style.top = Math.floor(Math.random() * 90 + 5) + "%"
-    btn.style.left = Math.floor(Math.random() * 90 + 5) + "%"
+    btn.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+    btn.style.left = Math.floor(Math.random() * 90 + 5) + "%";
 }
 
+// Evento cuando el mouse entra en el botón
 btnNo.addEventListener("mouseenter", function (e) {
-    moverAleatoriamente(e.target)
-})
+    moverAleatoriamente(e.target);
+});
+
+// Evento cuando se hace clic en el botón
+btnNo.addEventListener("click", function (e) {
+    moverAleatoriamente(e.target);
+});
